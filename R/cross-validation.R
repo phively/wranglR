4cfbd71e3b16119b630f05a29f5628da7ec24819
Paul Hively <pbhively@gmail.com>
2016-08-31 13:06:16 -0500

    k-fold cross-validation function

---
+++ R/cross-validation.R
@@ -0,0 +1,90 @@
+### Functions to assist with cross-validation
+
+#' K-fold Cross-Validation Samples
+#'
+#' This function takes a dataframe or vector and returns a list of indices for use in cross-validation.
+#' @param dat Data to split into cross-validation groups
+#' @param k Constant k for k-fold cross-validation; defaults to 2
+#' @param pct Percent of data to include in the first group; if not provided, defaults to equal-sized groups.
+#' Remaining groups must be at least size 1, and are as equally sized as possible, with any remainder included
+#' in the final group.
+#' @param seed Optional random seed to use for sampling
+#' @export
+#' @examples
+KFoldXVal <- function(dat, k=2, pct=NA, seed=NA) {
+  # Required number of indices is length(vector) or nrow(dat)
+  if (is.vector(dat)) {
+    n <- length(dat)
+  } else {
+    n <- nrow(dat)
+  }
+  ## Error-checking
+  {
+    # Check that k is an integer >= 2
+    if (k != round(k) | k < 2) {
+      stop(
+        paste("Invalid k, use an integer >= 2")
+      )
+    }
+    # Check that k <= n
+    if (k > n) {
+      stop(
+        paste("Invalid k, use k <= number of observations")
+      )
+    }
+    # Check that pct is in the range (0, 1)
+    if (!is.na(pct) && (0 >= pct | 1 <= pct)) {
+      stop(
+        paste("Invalid pct, must be a percent in the range 0 < pct < 1")
+      )
+    }
+    # Check that any present seed is numeric, and set it
+    if (!is.na(seed)[1]) {
+      if (!is.numeric(seed)) {
+        stop(
+          paste("Invalid seed, must be numeric")
+        )
+      } else {
+        set.seed(seed)
+      }
+    }
+  }
+  # Generate random list of indices
+  ind <- sample(1:n, size=n, replace=F)
+  # Determine the number of observations to be included in the first cross-validation group
+  if (!is.na(pct)){
+    # If pct was provided, use it to set the size of the first group
+    n.grp1 <- quantile(1:n, probs=pct)
+  } else {
+    # Otherwise the groups are equally sized
+    n.grp1 <- n/k
+  }
+  # Reduce n.grp1 if it doesn't leave enough for the other groups, decrease it and print a warning
+  if (n - n.grp1 < k - 1) {
+    pct.new <- (n - k + 1)/n
+    n.grp1 <- quantile(1:n, probs=pct.new)
+    warning(
+      paste("'pct = ", pct, "' resulted in size 0 groups; pct decreased to ", pct.new, sep="")
+    )
+  }
+  ## Create k-element list of indices
+  ## This method passes the "are the other permutations equally likely?" test
+  xval <- list()
+  # First group
+  end <- n.grp1
+  xval[[1]] <- ind[1:end] %>% na.omit()
+  # Middle groups
+  n.grp <- round((n - n.grp1)/(k-1))
+  if (k > 2) {
+    for (i in 2:(k-1)) {
+      start <- floor(end + 1)
+      end <- start + n.grp - 1
+      xval[[i]] <- ind[start:end] %>% na.omit()
+    }
+  }
+  # Last group
+  start <- floor(end + 1)
+  xval[[k]] <- ind[start:n] %>% na.omit()
+  # Return final output
+  return(xval)
+}
