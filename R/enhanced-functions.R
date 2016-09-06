60fc25b0688ad685f0c90de701d6b6605308eda9
Paul Hively <pbhively@gmail.com>
2016-09-06 18:14:30 -0500

    Libraries() function to load multiple at once

--- R/enhanced-functions.R
+++ R/enhanced-functions.R
@@ -17,15 +17,46 @@
 
 #' Load Packages
 #'
-#' This function loads
-#' @param input Libraries to be loaded
+#' This function attempts to load or install one or more packages. Adapted from
+#' \url{http://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them}
+#' @param package Package name to be loaded.
+#' @param \ldots Optional additional packages to load, to eliminate the need for c().
+#' @param install Whether to attempt to install missing packages from CRAN. Defaults to \code{TRUE}.
+#' @importFrom magrittr %>%
 #' @export
-#' #@examples
-#' Libraries(MASS)
-#'
-#' # Load two libraries at once
-#'
+#' @examples
 #' # Load libraries from a list
-Libraries <- function(input) {
-  stop(paste("To be implemented"))
+#' libs <- c("tools", "utils")
+#' Libraries(libs)
+#'
+#' # Load libraries directly
+#' Libraries("Matrix", "MASS")
+#'
+#' # Warning when loading an unavailable package
+#' Libraries("StrKcaQYrvg", install=FALSE)
+Libraries <- function(package, ..., install=TRUE) {
+  # If no arguments passed
+  if (missing(package)) {
+    stop("Must enter at least one package")
+  }
+  # If dot names were passed, append to end of package
+  addl <- list(...) %>% as.character()
+  if (length(addl) > 0) {
+    package <- c(package, addl)
+  }
+  # Determine which packages (if any) are not installed
+  not.found <- package[package %nin% utils::installed.packages()[,"Package"]]
+  # Install needed packages
+  if (length(not.found) > 0 & install) {
+    pkgs <- paste(not.found, sep="", collapse="', '")
+    message(
+      paste("Attempting to install '", pkgs, "'", sep="")
+    )
+    utils::install.packages(not.found)
+  }
+  # Sequentially load packages
+  for (p in package) {
+    # Attempt to load the package
+    require(p, character.only=TRUE)
+  }
 }
